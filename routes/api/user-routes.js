f886af6969b06830cf0ca5b2186fb368134744ab
Maria Meyer <mbean1216@icloud.com>
2022-06-05 17:07:32 -0500

    pseudo coding user and thought routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,20 @@
+/* api/users */
+
+// GET all users
+// GET a single user by it's _id and pupulated thought and friend data
+// POST a new user
+/* 
+example data
+    {
+        "username": "lernantino",
+        'email': 'lernantion@gmail.com'
+    }
+*/
+// PUT to update  a user by its _d
+// DELETE to remove user by its _id
+// ***BONUS*** remove a user's associated thoughts when deleted
+
+/* /api/users/:userId/friends/:friendId */
+
+// POST to add a new friend to a user's friend list
+// DELETE to remove a friend from a user's friend list
