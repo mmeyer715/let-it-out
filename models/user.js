39837c12545d98970220a57b418fbd7f3693a4f1
Maria Meyer <mbean1216@icloud.com>
2022-06-05 16:02:30 -0500

    adding starting files and creating user model

---
+++ models/user.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose');
+
+
+// Creating user schema
+
+const UserSchema = new Schema(
+
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /.+\@.+\..+/
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+// create User model using UserSchema
+const User = model('User', UserSchema);
+
+// export model
+module.exports = User;
